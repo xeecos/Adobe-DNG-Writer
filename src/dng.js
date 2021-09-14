302d623f6c53001945388354364a0f8e2476f0fc
虎子哥 <flashindream@gmail.com>
2021-09-14 09:28:19 +0800

    update

--- src/dng.js
+++ src/dng.js
@@ -24,43 +24,15 @@ class DNG
     
     static pack10(data)
     {
-        out = np.zeros((data.shape[0], int(data.shape[1]*(1.25))), dtype=np.uint8)
-        out[:, ::5] = data[:, ::4] >> 2
-        out[:, 1::5] = ((data[:, ::4] & 0b0000000000000011) << 6)
-        out[:, 1::5] += data[:, 1::4] >> 4
-        out[:, 2::5] = ((data[:, 1::4] & 0b0000000000001111) << 4)
-        out[:, 2::5] += data[:, 2::4] >> 6
-        out[:, 3::5] = ((data[:, 2::4] & 0b0000000000111111) << 2)
-        out[:, 3::5] += data[:, 3::4] >> 8
-        out[:, 4::5] = data[:, 3::4] & 0b0000000011111111
-        return out
+        return data;
     }
-
-
     static pack12(data)
     {
-        out = np.zeros((data.shape[0], int(data.shape[1]*(1.5))), dtype=np.uint8)
-        out[:, ::3] = data[:, ::2] >> 4
-        out[:, 1::3] = ((data[:, ::2] & 0b0000000000001111) << 4)
-        out[:, 1::3] += data[:, 1::2] >> 8
-        out[:, 2::3] = data[:, 1::2] & 0b0000001111111111
-        return out
+        return data;
     }
     static pack14(data)
     {
-        out = np.zeros((data.shape[0], int(data.shape[1]*(1.75))), dtype=np.uint8)
-        out[:, ::7] = data[:, ::6] >> 6
-        out[:, 1::7] = ((data[:, ::6] & 0b0000000000000011) << 6)
-        out[:, 1::7] += data[:, 1::6] >> 8
-        out[:, 2::7] = ((data[:, 1::6] & 0b0000000000001111) << 4)
-        out[:, 2::7] += data[:, 2::6] >> 6
-        out[:, 3::7] = ((data[:, 2::6] & 0b0000000000111111) << 2)
-        out[:, 3::7] += data[:, 3::6] >> 8
-        out[:, 4::7] = ((data[:, 3::6] & 0b0000000000001111) << 4)
-        out[:, 4::7] += data[:, 4::6] >> 6
-        out[:, 5::7] = ((data[:, 4::6] & 0b0000000000111111) << 2)
-        out[:, 5::7] += data[:, 5::6] >> 8
-        out[:, 6::7] = data[:, 5::6] & 0b0000000011111111
+        return data;
     }
     static convert(image,tags,name,path)
     {
@@ -72,28 +44,28 @@ class DNG
         let bpp = tags.get(TagType.BitsPerSample)[0]
         if(bpp == 8)
         {
-            tile = rawFrame.astype('uint8').tobytes()
+            tile = rawFrame;
         }
         else if(bpp == 10)
         {
-            tile = pack10(rawFrame).tobytes()
+            tile = pack10(rawFrame);
         }
         else if(bpp == 12)
         {
-            tile = pack12(rawFrame).tobytes()
+            tile = pack12(rawFrame);
         }
         else if(bpp == 14)
         {
-            tile = pack14(rawFrame).tobytes()
+            tile = pack14(rawFrame);
         }
         else if(bpp == 16)
         {
-            tile = rawFrame.tobytes()
+            tile = rawFrame;
         }
 
         dngTemplate.ImageDataStrips.append(tile)
-        mainIFD = new IFD()
-        mainTagStripOffset = new Tag(TagType.TileOffsets, [0 for tile in dngTemplate.ImageDataStrips])
+        let mainIFD = new IFD()
+        let mainTagStripOffset = new Tag(TagType.TileOffsets, [0 for tile in dngTemplate.ImageDataStrips])
         mainIFD.tags.append(mainTagStripOffset)
         mainIFD.tags.append(new Tag(TagType.NewSubfileType, [0]))
         mainIFD.tags.append(new Tag(TagType.TileByteCounts, [len(tile) for tile in dngTemplate.ImageDataStrips]))
@@ -112,7 +84,7 @@ class DNG
 
         mainTagStripOffset.setValue([k for offset, k in dngTemplate.StripOffsets.items()])
 
-        buf = bytearray(totalLength)
+        buf = Buffer.alloc(totalLength)
         dngTemplate.setBuffer(buf)
         dngTemplate.write()
 
--- src/ifd.js
+++ src/ifd.js
@@ -1,9 +1,7 @@
-      
 class IFD
 {
     constructor()
     {
-
         this.tags = []
         this.NextIFDOffset = 0
     }
@@ -14,33 +12,29 @@ class IFD
         this.offset = offset
         currentDataOffset = offset + 2 + this.tags.length*12 + 4
         currentTagOffset = offset + 2
-        for tag in sorted(self.tags, key=lambda x: x.TagId)
-        {
-            tag.setBuffer(buf, currentTagOffset, currentDataOffset)
-            currentTagOffset += 12
-            currentDataOffset += tag.dataLen()
-            currentDataOffset = (currentDataOffset + 3) & 0xFFFFFFFC
-        }
+        this.tags.forEach(tag=>{
+            tag.setBuffer(buf, currentTagOffset, currentDataOffset);
+            currentTagOffset += 12;
+            currentDataOffset += tag.dataLen();
+            currentDataOffset = (currentDataOffset + 3) & 0xFFFFFFFC;
+        })
     }
     dataLen(self)
     {
         totalLength = 2 + len(self.tags)*12 + 4
-        for tag in sorted(self.tags, key=lambda x: x.TagId):
+        this.tags.forEach(tag=>{
             totalLength += tag.dataLen()
+        });
         return (totalLength + 3) & 0xFFFFFFFC
     }
 
     write()
     {
-        if not self.buf:
-            raise RuntimeError("buffer not initialized")
-
-        struct.pack_into("<H", self.buf, self.offset, len(self.tags))
-
-        for tag in sorted(self.tags, key=lambda x: x.TagId):
-            tag.write()
-
-        struct.pack_into("<I", self.buf, self.offset + 2 + len(self.tags)*12, self.NextIFDOffset)
+        this.buf.writeUInt16LE(this.tags.length,this.offset);
+        this.tags.forEach(tag=>{
+            tag.write();
+        });
+        this.buf.writeUInt32LE(this.offset + 2 + (this.tags.length)*12,this.NextIFDOffset);
     }
 }
 module.exports = IFD;
